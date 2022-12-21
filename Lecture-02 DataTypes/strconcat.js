70c0b445e27d0db86f472e1941e7b2e7ac92a600
Your Name <you@example.com>
2022-12-21 09:56:43 +0700

    create strconcat.js

--- Lecture-02 DataTypes/strconcat.js
+++ Lecture-02 DataTypes/strconcat.js
@@ -4,3 +4,11 @@ let str2 = "World!";
 //Using the + operator
 console.log("********Using the + operator********\n");
 console.log(str1 + str2);
+console.log(str1 + "Big" + str2);
+
+console.log("\n********Adding Spacing********\n")
+//Adding Spacing
+str1 = "Hello ";
+
+console.log(str1 + str2);
+console.log(str1 + "Big" + str2)
