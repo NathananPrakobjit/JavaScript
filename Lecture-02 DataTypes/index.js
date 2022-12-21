c80a5d0cbcb22111cc52d2e0435f9b2998327dff
Your Name <you@example.com>
2022-12-21 09:48:03 +0700

    create datatypes.js

---
+++ Lecture-02 DataTypes/index.js
@@ -0,0 +1,11 @@
+const message = "Hello";
+const palce = "World";
+
+// logging out string
+console.log('Hello, World!');
+
+// using subtitutions
+console.log('Hello, %s!0', place);
+
+//using a string literal
+console.log('${message}, ${place}!');
---
+++ Lecture-02 DataTypes/strconcat.js
@@ -0,0 +1,6 @@
+let str1 = "Hello";
+let str2 = "World!";
+
+//Using the + operator
+console.log("********Using the + operator********\n");
+console.log(str1 + str2);
