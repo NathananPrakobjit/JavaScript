7eca07c5f4126bb93cf791aee52ea73415267e2a
Your Name <you@example.com>
2022-12-21 09:56:24 +0700

    update

--- Lecture-02 DataTypes/index.js
+++ Lecture-02 DataTypes/index.js
@@ -1,11 +1,11 @@
 const message = "Hello";
 const palce = "World";
 
-// logging out string
+// logging out a string
 console.log('Hello, World!');
 
-// using subtitutions
-console.log('Hello, %s!0', place);
+// using substitutions
+console.log('Hello, %s!', place);
 
 //using a string literal
 console.log('${message}, ${place}!');
