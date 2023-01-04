823d6b43aab53beb07a45616af0befb6ea6029d5
Your Name <you@example.com>
2023-01-04 11:10:52 +0700

    update

---
+++ Lecture-03-ArrayLoopObject/JSON-Obj/objDemo.js
@@ -0,0 +1,26 @@
+/* START */
+
+// How can you represent real-world objects in code?
+// They have associated attributes = object PROPERTIES
+// They have associated actions = object METHIODS
+// They have associated context = "this"
+
+
+// First:
+//Let's define a simple object (with no properties or methods)
+const blank = {};
+console.log("Blank Type:",typeof blank);
+console.log("Blank value",Blank);
+
+
+/*
+// Next:
+// Let's define an object with PROPERTIES (attributes)
+// Obhject are jest a collection of name-value pairs
+// separated by commas
+const book = {
+    title : "1984",
+    author: "Geroge Orwell",
+    isAvailable: false
+};
+*/
